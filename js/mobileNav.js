7a923dc71c13a37893eb223b0cf836e677f40b7c
Maruth Goyal <maruthgoyal@gmail.com>
2015-09-20 14:52:33 +0530

    Design update

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -4,18 +4,8 @@
 
             if(screenWidth < 1024){
 
-                var myImg = document.createElement("IMG");
-
-                myImg.setAttribute("src", "menuicon.png");
-                myImg.setAttribute("id", "navImg");
-                myImg.style.width = "55px";
-                myImg.style.height = "auto";
-
-                document.getElementById("myNav").appendChild(myImg);
-
-                document.getElementById("nonMobileList").style.display = "none";
-                document.getElementById("ic").style.display = "none"; 
-                document.getElementById("myNav").style.height = "60px";
+                document.getElementById('mainSheet').setAttribute('href', 'mobileStyle.css')
+                console.log("true")
             }
 
           
