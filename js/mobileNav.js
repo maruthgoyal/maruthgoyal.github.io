8bfa6fb83bea530225a3793442382eb442474cbb
Maruth Goyal <maruthgoyal@gmail.com>
2015-09-20 15:14:55 +0530

    Portrait/Landscape adjustment
    Changes according to orientation, fixed sizing

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -2,14 +2,23 @@
             
             var screenWidth = window.outerWidth;
 
-            if(screenWidth < 1024){
+            var screenInnerWidt = window.innerWidth;
+            var screenInnderH = window.innerHeight;
+
+            if(screenWidth < 1025){
 
                 document.getElementById('mainSheet').setAttribute('href', 'mobileStyle.css')
                 console.log("true")
+
+                if(screenInnerWidt < screenInnderH){
+                	document.getElementById('bigIcon').style.width = "55vw"
+                }
             }
             
             else{
+
               document.getElementById('mainSheet').setAttribute('href', 'stylesheet.css')
+
             }
 
           
